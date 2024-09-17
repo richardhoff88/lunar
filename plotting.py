24bdcc89a560b6d7f027482124b44b68fef6d2ef
richardhoff88 <richardhoff88@gmail.com>
2024-09-17 13:32:03 -0700

    batter and total solar plots

--- plotting.py
+++ plotting.py
@@ -12,6 +12,8 @@ reader = csv.DictReader(f)
 
 solar_x = []
 solar_y = []
+solar_z = []
+battery = []
 time = []
 
 
@@ -19,20 +21,29 @@ for col in reader:
     time.append(col["Satellite Date/Time UTC"])
     solar_x.append(col['Solar Panel Voltage X mV'])
     solar_y.append(col['Solar Panel Voltage Y mV'])
+    solar_z.append(col['Solar Panel Voltage Z mV'])
+    battery.append(col['Battery Voltage mV'])
+
+# plt.figure()
+# plt.plot(time, solar_x, label="X")
+# plt.plot(time, solar_y, label="Y")
+# plt.plot(time, solar_z, label="Z")
+# plt.legend()
+# plt.title("Total Solar Panel Voltage over Time")
+# plt.xlabel("Time")
+# plt.ylabel("Voltage (mV)")
+# plt.show()
+
+# plt.plot(time, solar_y)
+# plt.title("Solar Panel Voltage Y over Time")
+# plt.xlabel("Time")
+# plt.ylabel("Voltage (mV)")
 
 plt.figure()
-plt.plot(time, solar_x, label=None)
-plt.title("Solar Panel Voltage X over Time")
+plt.plot(time, battery)
+plt.title("Battery Voltage over Time")
 plt.xlabel("Time")
 plt.ylabel("Voltage (mV)")
-plt.show()
-
-plt.figure()
-plt.plot(time, solar_y)
-plt.title("Solar Panel Voltage Y over Time")
-plt.xlabel("Time")
-plt.ylabel("Voltage (mV)")
-
 plt.show()
 
 
